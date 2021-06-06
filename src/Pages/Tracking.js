65d7fc056210e8e72eef51a563a5cb2b9d345aa4
sogatanco <adeliapucha46@gmail.com>
2021-06-06 22:16:02 +0700

    update firebase

--- src/Pages/Tracking.js
+++ src/Pages/Tracking.js
@@ -41,12 +41,12 @@ class Tracking extends React.Component{
                 <div>
                      <Navbar bg="dark" variant="dark" expand="lg">
                         <Container>
-                            <Navbar.Brand href="#home">PromoSteem.COM (powerup information)</Navbar.Brand>
+                            <Navbar.Brand href="#home">PromoSteem.com</Navbar.Brand>
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
                                     <Nav className="me-auto">
-                                    {/* <Nav.Link href="#home">Home</Nav.Link>
-                                    <Nav.Link href="#link">Link</Nav.Link> */}
+                                    <Nav.Link href="">Power up info</Nav.Link>
+                                    {/* <Nav.Link href="#link">Link</Nav.Link> */}
                                     </Nav>
                                 </Navbar.Collapse>
                         </Container>
